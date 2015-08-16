aa196e653d51e3a0f6c9469c4952594e53062983
gameslayer <cdfrosty@gmail.com>
2015-08-16 12:58:15 +1000

    Update Unreal_Engine_Install_Script.sh

--- Unreal_Engine_Install_Script.sh
+++ Unreal_Engine_Install_Script.sh
@@ -1,8 +1,4 @@
-#Unrela Engine Install Script Version 1.0 Ubuntu Derivatives
-
-echo Installing depenancies: &&
-#Dependacies
-sudo apt-get install mono-mcs && sudo apt-get install mono-gmcs && sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF && echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list && sudo apt-get update && sudo apt-get install mono-gmcs mono-xbuild mono-dmcs libmono-corlib4.0-cil libmono-system-data-datasetextensions4.0-cil libmono-system-web-extensions4.0-cil libmono-system-management4.0-cil libmono-system-xml-linq4.0-cil cmake dos2unix clang-3.3 libfreetype6-dev libsdl2-dev libgtk-3-dev libmono-microsoft-build-tasks-v4.0-4.0-cil xdg-user-dirs && sudo apt-get install mono-devel &&
+#Unreal Engine Install Script Version 1.1 Ubuntu Derivatives
 
 echo Preparing Graphics For Unreal Engine &&
 #Graphics For Unreal Engine
@@ -19,3 +15,4 @@ find Engine/Source/Programs/AutomationTool -name "*Automation.csproj" -exec sed 
 echo Cleaning up and Enhancing the Makefile &&
 #Cleaning up and Enhancing the Makefile
 make UE4Editor ARGS=-clean
+
